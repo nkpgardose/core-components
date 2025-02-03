a9f74f1860c1cc297c8c87d47e94169c8a91ba97
Neil Gardose <neil@driva.com.au>
2025-02-03 22:01:57 +1100

    Initial commit; add two core component Button, Text

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import { HTMLProps, ReactNode } from "react";
+import styles from './Button.module.css'
+
+interface ButtonProps extends HTMLProps<HTMLButtonElement> {
+  /**
+   * Any elements or text or both. Usually children will handle icon and text
+   * combination.
+   */
+  children: ReactNode
+  /**
+   * The type of the button.
+   * Default is button type.
+   */
+  type?: 'button' | 'submit' | 'reset' | undefined
+  /**
+   * Different button styles primarly on colours.
+   */
+  variant?: 'primary' | 'secondary' | ''
+}
+
+function Button({
+  children,
+  variant = '',
+  type = 'button',
+  ...remainingProps
+}: ButtonProps) {
+  const styleResults = `${styles.Button} ${styles[variant]}`.trim();
+  
+  return (
+    <button className={styleResults} type={type} {...remainingProps}>
+      {children}
+    </button>
+  )
+}
+
+export default Button;
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,61 @@
+import styles from './Text.module.css'
+import { ReactNode } from 'react'
+
+interface TextProps {
+	id?: string | undefined
+	/**
+	 * Any elements, texts, or both. Usually children will handle icon and text
+	 * combination.
+	 */
+	children: ReactNode
+	/**
+	 * Accepts string for additional styles for the text i.e spacing, colours, etc.
+	 */
+	className?: string
+	/**
+	 * html text tags
+	 */
+	HTMLElement?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span'
+	/**
+	 * Different styles of the text.
+	 */
+	variant?:
+		| 'body'
+		| 'hero'
+		| 'intro'
+		| 'largeTitle'
+		| 'title'
+		| 'title2'
+		| 'thinTitle2'
+		| 'title3'
+		| 'headline'
+		| 'subhead'
+		| 'footnote'
+		| 'caption'
+	/**
+	 * Text alignment like as center, by default it's left aligned
+	 */
+	alignment?: 'left' | 'center' | 'right'
+}
+
+function Text({
+	children,
+	className = '',
+	HTMLElement = 'p',
+	variant = 'body',
+	alignment = 'left',
+	...remainingProps
+}: TextProps) {
+	return (
+		<HTMLElement
+			{...remainingProps}
+			className={[styles.Text, styles[variant], styles[alignment], className]
+				.join(' ')
+				.trim()}
+		>
+			{children}
+		</HTMLElement>
+	)
+}
+
+export default Text
