b38699b1a433064bdac287b40179857745acdf7e
Neil Gardose <neil@driva.com.au>
2025-02-05 11:38:24 +1100

    feat: add new color variatn in Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -15,7 +15,7 @@ interface ButtonProps extends HTMLProps<HTMLButtonElement> {
   /**
    * Different button styles primarly on colors.
    */
-  variant?: 'primary' | 'secondary' | ''
+  variant?: 'primary' | 'secondary' | 'tertiary' | ''
 }
 
 function Button({
